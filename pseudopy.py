2b630ec5ce186402fcb535ff9afbacbd8c4a8518
JokeNeverSoke <45055133+JokeNeverSoke@users.noreply.github.com>
2019-10-10 20:44:41 +0800

    work in progress (wip)

---
+++ pseudopy.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+
+import sys
+import string
+import argparse
+
+def pystrtolist(code):
+    flist = []
+    current_group = []
+    line_continuation = False
+    for char in code:
+        if char in string.ascii_letters + ["_"]:
+            current_group.append(char)
+        elif char in ["\n", " "]:
+            if current_group:
+                flist.append(current_group)
+                current_group = []
+        elif char == "\\":
+            line_continuation = True
+                
+def py2pseudo(string:str):
+    fstring = string.strip()
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            "pseudopy"
+            description="A parser/compiler of Python <==> Pseudocode for saving IG/AS/AL students (i hope)"
+            )
+    parser.add_argument("-i", "--indent", nargs="?", default=4, type=int)
+    arguments = parser.parse_args(sys.argv[1:])
+    print(arguments.indent)
